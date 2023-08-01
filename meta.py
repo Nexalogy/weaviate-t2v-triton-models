7fe2244d21d2a9ee5140b087a99f777913a5906a
Hezekiah Akiva Bacovcin <abacovcin@nexalogy.com>
2023-08-01 15:03:38 -0400

    Intialize db

--- meta.py
+++ meta.py
@@ -4,20 +4,12 @@ from transformers import AutoConfig
 class Meta:
     config: AutoConfig
 
-    def __init__(self, model_path):
-        self.config = AutoConfig.from_pretrained(model_path)
+    def __init__(self, modelName, tritonUrl):
+		 self.model_name = modelName
+		 self.tritonUrl = tritonUrl
 
     def get(self):
-        return {
-            'model': self.config.to_dict()
-        }
-
-    def getModelType(self):
-        return self.config.to_dict()['model_type']
-
-    def get_architecture(self):
-        architecture = None
-        conf = self.config.to_dict()
-        if "architectures" in conf:
-            architecture = conf["architectures"][0]
-        return architecture
+		 return {
+			 'model_name': self.modelName,
+			 'triton_url': self.tritonUrl,
+		 }
